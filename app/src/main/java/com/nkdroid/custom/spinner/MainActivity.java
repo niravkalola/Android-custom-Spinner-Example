48fb2ccef29db5b0adf7b123afb9287c257369da
nkdroidsolutions <nkdroidsolutions@gmail.com>
2016-02-20 21:16:45 +0530

    Initial commit

---
+++ app/src/main/java/com/nkdroid/custom/spinner/MainActivity.java
@@ -0,0 +1,160 @@
+package com.nkdroid.custom.spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Simple Spinner Android Example
+        initSimpleSpinner();
+
+        //Android Custom Spinner Example Programmatically
+        initCustomSpinner();
+
+    }
+
+    private void initCustomSpinner() {
+
+        Spinner spinnerCustom= (Spinner) findViewById(R.id.spinnerCustom);
+        // Spinner Drop down elements
+        ArrayList<String> languages = new ArrayList<String>();
+        languages.add("Andorid");
+        languages.add("IOS");
+        languages.add("PHP");
+        languages.add("Java");
+        languages.add(".Net");
+        CustomSpinnerAdapter customSpinnerAdapter=new CustomSpinnerAdapter(MainActivity.this,languages);
+        spinnerCustom.setAdapter(customSpinnerAdapter);
+        spinnerCustom.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                String item = parent.getItemAtPosition(position).toString();
+
+                Toast.makeText(parent.getContext(), "Android Custom Spinner Example Output..." + item, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void initSimpleSpinner() {
+        // Spinner element
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+
+
+        // Spinner Drop down elements
+        List<String> languages = new ArrayList<String>();
+        languages.add("Andorid");
+        languages.add("IOS");
+        languages.add("PHP");
+        languages.add("Java");
+        languages.add(".Net");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, languages);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                String item = parent.getItemAtPosition(position).toString();
+
+                Toast.makeText(parent.getContext(), "Android Simple Spinner Example Output..." + item, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+
+
+    public class CustomSpinnerAdapter extends BaseAdapter implements SpinnerAdapter {
+
+        private final Context activity;
+        private ArrayList<String> asr;
+
+        public CustomSpinnerAdapter(Context context,ArrayList<String> asr) {
+            this.asr=asr;
+            activity = context;
+        }
+
+
+
+        public int getCount()
+        {
+            return asr.size();
+        }
+
+        public Object getItem(int i)
+        {
+            return asr.get(i);
+        }
+
+        public long getItemId(int i)
+        {
+            return (long)i;
+        }
+
+
+
+        @Override
+        public View getDropDownView(int position, View convertView, ViewGroup parent) {
+            TextView txt = new TextView(MainActivity.this);
+            txt.setPadding(16, 16, 16, 16);
+            txt.setTextSize(18);
+            txt.setGravity(Gravity.CENTER_VERTICAL);
+            txt.setText(asr.get(position));
+            txt.setTextColor(Color.parseColor("#000000"));
+            return  txt;
+        }
+
+        public View getView(int i, View view, ViewGroup viewgroup) {
+            TextView txt = new TextView(MainActivity.this);
+            txt.setGravity(Gravity.CENTER);
+            txt.setPadding(16, 16, 16, 16);
+            txt.setTextSize(16);
+            txt.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_down, 0);
+            txt.setText(asr.get(i));
+            txt.setTextColor(Color.parseColor("#000000"));
+            return  txt;
+        }
+
+    }
+}
